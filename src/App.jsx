af4463ac200d2787484cde4aa8141814a925e85d
HP <awadhrajpatel95@gmail.com>
2025-01-23 15:49:22 +0530

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,10 @@ import UserProtectWrapper from './Pages/UserProtectWrapper'
 import UserLogout from './Pages/UserLogout'
 import CaptainHome from './Pages/CaptainHome'
 import CaptainProtectWrapper from './Pages/CaptainProtectWrapper'
+import Riding from './Pages/Riding'
+import CaptainRiding from './Pages/CaptainRiding'
+import 'remixicon/fonts/remixicon.css'
+
 
 const App = () => {
   return (
@@ -17,6 +21,8 @@ const App = () => {
       <Routes>
       <Route path='/' element={<Start/>} />
       <Route path='/login' element={<UserLogin />} /> 
+      <Route path='/riding' element={<Riding/>} />
+      <Route path='/captain-riding' element={<CaptainRiding/>} />  
       <Route path='/signup' element={<UserSignup />} /> 
       <Route path='/captain-login' element={<Captainlogin />} /> 
       <Route path='/captain-signup' element={<CaptainSignup />} />
